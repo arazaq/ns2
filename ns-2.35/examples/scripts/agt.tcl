407da6ab64d7d15ff0079350b65ee75d31ba0dbc
arazaq <arazaq@users.noreply.github.com>
2015-08-20 17:28:08 +0100

    Update agt.tcl

--- ns-2.35/examples/scripts/agt.tcl
+++ ns-2.35/examples/scripts/agt.tcl
@@ -3,9 +3,9 @@ set glagent [new Agent/AgentGLTcl]
 
 # Pass command to Agent
 #puts stdout "Call to execute NS"
-#$glagent "call-execute-ns" "/home/ali/apps/ns2-master/ns-2.35/examples/test01/lantest_example.tcl"
-$glagent "call-execute-ns" "/home/ali/apps/ns2-master/ns-2.35/examples/test01/01lantest_example.tcl"
+#$glagent "call-execute-ns" "/home/apps/ns2-master/ns-2.35/examples/test01/lantest_example.tcl"
+$glagent "call-execute-ns" "/home/apps/ns2-master/ns-2.35/examples/test01/01lantest_example.tcl"
 
 #puts stdout "Call to execute GL"
-$glagent "call-execute-gl" "/home/ali/apps/ns2-master/ns-2.35/examples/test01/02_test_PMSG_windturb_GCU.glm"
+$glagent "call-execute-gl" "/home/apps/ns2-master/ns-2.35/examples/test01/02_test_PMSG_windturb_GCU.glm"
 
